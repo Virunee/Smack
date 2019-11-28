5f7d831ff72cfa1a117beb78e609e2455e8717c3
Virunee Viravong <viravongv@gmail.com>
2019-11-28 14:13:20 +0000

    lesson 15
    Add login functionality

--- app/src/main/java/com/example/smack/Controller/LoginActivity.kt
+++ app/src/main/java/com/example/smack/Controller/LoginActivity.kt
@@ -4,7 +4,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import com.example.smack.R
+import com.example.smack.Services.AuthService
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
@@ -14,7 +17,18 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun loginLoginBtnClicked(view: View) {
-
+        val email = loginEmailText.text.toString()
+        val password = passwordEmailText.text.toString()
+        AuthService.loginUser(this, email, password) { loginSuccess ->
+            if(loginSuccess) {
+                AuthService.finderUserByEmail(this) { findSuccess ->
+                    if(findSuccess) {
+                        Toast.makeText(this, "Login successful", Toast.LENGTH_SHORT).show()
+                        finish()
+                    }
+                }
+            }
+        }
     }
 
     fun createUserBtnClicked(view: View) {
